8805fb57a4afee57a318193644d5b36ecd9456fa
Alex <aeversme@gmail.com>
2021-12-04 21:18:25 -0500

    complete dec04-01

--- dec04/dec04-01.py
+++ dec04/dec04-01.py
@@ -1,4 +1,4 @@
-with open('test_bingo.txt') as b:
+with open('bingo.txt') as b:
     bingo = b.readlines()
 
 
@@ -14,7 +14,7 @@ def convert_bingo_input():
 def make_bingo_cards(bingo_input):
     card_list = []
     index = 0
-    for i in range(int(len(bingo_input) / 5)):
+    for i in range(int(len(bingo_input) // 5)):
         card = []
         for j in range(5):
             row = bingo_input[index + j].lstrip().replace('  ', ' ').split(' ')
@@ -24,20 +24,73 @@ def make_bingo_cards(bingo_input):
     return card_list
 
 
+def look_for_number(draw, cards):
+    for card in range(len(cards)):
+        for row in range(len(cards[0])):
+            for element in range(len(cards[0][0])):
+                if cards[card][row][element] == draw:
+                    cards[card][row][element] = -1
+
+
+def look_for_bingo(cards):
+    for card in range(len(cards)):
+        for row in range(5):
+            is_bingo = True
+            for element in range(5):
+                if cards[card][row][element] != -1:
+                    is_bingo = False
+            if is_bingo:
+                return cards[card]
+        for column in range(5):
+            is_bingo = True
+            for element in range(5):
+                if cards[card][element][column] != -1:
+                    is_bingo = False
+            if is_bingo:
+                return cards[card]
+    return None
+
+
+def score_winning_card(card, winning_number):
+    unmarked_sum = 0
+    for row in range(5):
+        for element in range(5):
+            if card[row][element] != -1:
+                unmarked_sum += card[row][element]
+    return unmarked_sum * winning_number
+
+
 def squid_bingo():
     draw, card_strings = convert_bingo_input()
     cards = make_bingo_cards(card_strings)
     print(draw)
     print(cards[0])
 
-    # while ! winner
-    # get next draw number
-    # loop through every card to look for draw number
-    # if number found on card, replace with -1
-    # increment a # of draws counter
-    # if number of draws >= 5, check for winner
-    # if winner, do calculation
-    # else continue
+    winner = False
+    number_of_draws = 1
+    while not winner:
+        draw_number = draw[number_of_draws - 1]
+        print(f"draw_number: {number_of_draws}, number: {draw_number}")
+        look_for_number(draw_number, cards)
+        number_of_draws += 1
+        if 5 < number_of_draws <= len(draw):
+            result = look_for_bingo(cards)
+            if result is not None:
+                winner = True
+                print("Game over, there's a winning card!")
+                print(f"Winning number: {draw_number}")
+                print(f"Winning card:\n{result}")
+                score = score_winning_card(result, draw_number)
+                print(f"The winning card's score is: {score}")
+            else:
+                print("No winner yet.")
+                continue
+        elif number_of_draws > len(draw):
+            print("All numbers called, no winner.")
+            break
+        else:
+            print("Not enough draws to have a bingo yet.")
+            continue
 
 
 squid_bingo()
