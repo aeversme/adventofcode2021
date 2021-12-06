56bad8a2476179b2e990a781e06ea19237be2c39
Alex <aeversme@gmail.com>
2021-12-05 20:12:05 -0500

    add diagonal line plotting

--- dec05/dec05_02.py
+++ dec05/dec05_02.py
@@ -24,28 +24,51 @@ def find_max_value(vent_list):
     return max_value
 
 
-# TODO: for diagonal lines, condition is:
-# TODO: abs(coord_list[2] - coord_list[0]) == abs(coord_list[3] -coord_list[1])
-def is_line(coord_list):
-    return coord_list[0] == coord_list[2] or coord_list[1] == coord_list[3]
+def check_line_type(coord_list):
+    x1 = coord_list[0]
+    x2 = coord_list[2]
+    y1 = coord_list[1]
+    y2 = coord_list[3]
+    if x1 == x2:
+        return "vertical"
+    elif y1 == y2:
+        return "horizontal"
+    elif abs(x1 - x2) == abs(y1 - y2):
+        if (x1 < x2 and y1 > y2) or (x1 > x2 and y1 < y2):
+            return "negative_diag"
+        else:
+            return "positive_diag"
+    else:
+        return None
 
 
-# TODO: define a way to plot diagonal lines
-def plot_line(coord_list, plot):
-    # vertical line
-    if coord_list[0] == coord_list[2]:
+def plot_line(line_type, coord_list, plot):
+    if line_type == "vertical":
         x_start = coord_list[0]
         y_start = min(coord_list[1], coord_list[3])
         for _ in range(abs(coord_list[1] - coord_list[3]) + 1):
             plot[y_start][x_start] += 1
             y_start += 1
-    # horizontal line
-    if coord_list[1] == coord_list[3]:
+    elif line_type == "horizontal":
         x_start = min(coord_list[0], coord_list[2])
         y_start = coord_list[1]
         for _ in range(abs(coord_list[0] - coord_list[2]) + 1):
             plot[y_start][x_start] += 1
             x_start += 1
+    elif line_type == "positive_diag":
+        x_start = min(coord_list[0], coord_list[2])
+        y_start = min(coord_list[1], coord_list[3])
+        for _ in range(abs(coord_list[0] - coord_list[2]) + 1):
+            plot[y_start][x_start] += 1
+            x_start += 1
+            y_start += 1
+    else:
+        x_start = min(coord_list[0], coord_list[2])
+        y_start = max(coord_list[1], coord_list[3])
+        for _ in range(abs(coord_list[0] - coord_list[2]) + 1):
+            plot[y_start][x_start] += 1
+            x_start += 1
+            y_start -= 1
 
 
 def count_overlaps(plot):
@@ -57,16 +80,16 @@ def count_overlaps(plot):
     return overlap_count
 
 
-# TODO: include logic to plot diagonal lines
 def analyze_vents():
     vents_list = convert_input(vents)
     max_value = find_max_value(vents_list)
-    print(vents_list)
-    print(max_value)
+    # print(vents_list)
+    # print(max_value)
     plot = [[0] * (max_value + 1) for _ in range(max_value + 1)]
     for vent in vents_list:
-        if is_line(vent):
-            plot_line(vent, plot)
+        line_type = check_line_type(vent)
+        if line_type is not None:
+            plot_line(line_type, vent, plot)
     # print(plot)
     print(f"Number of vent overlaps: {count_overlaps(plot)}")
     return 0
