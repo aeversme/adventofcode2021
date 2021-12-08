3a17972c97ff0c26c517faefc0e155951bb51bad
Alex <aeversme@gmail.com>
2021-12-08 13:14:33 -0500

    refactor and document input_handler.py

--- dec08/input_handler.py
+++ dec08/input_handler.py
@@ -5,19 +5,23 @@ def convert_input(data):
 
 
 def convert_for_part_two(data):
+    """
+    Takes a list of strings. Returns two lists of lists of strings.
+    """
     data_strip_lines = [i.strip('\n') for i in data]
     data_split = [i.split(' | ') for i in data_strip_lines]
     data_input = [i[0] for i in data_split]
-    data_input_sas = split_and_sort(data_input, 10)
     data_output = [i[1] for i in data_split]
-    data_output_sas = split_and_sort(data_output, 4)
-    return data_input_sas, data_output_sas
+    return split_and_sort(data_input), split_and_sort(data_output)
 
 
-def split_and_sort(string_list, r):
+def split_and_sort(string_list):
+    """
+    Takes a list of strings. For each string, splits the string and sorts the resulting strings alphabetically.
+    """
     string_list_split = [i.split(' ') for i in string_list]
     for i in string_list_split:
-        for j in range(r):
+        for j in range(len(i)):
             i[j] = sorted(i[j])
-            i[j] = "".join(i[j])
+            i[j] = ''.join(i[j])
     return string_list_split
