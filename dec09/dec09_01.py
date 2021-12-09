99751cf8f52c1b917d2ebcbd5ab80327e875fa29
Alex <aeversme@gmail.com>
2021-12-09 13:35:12 -0500

    update completed dec09_01

--- dec09/dec09_01.py
+++ dec09/dec09_01.py
@@ -1,22 +1,19 @@
 from input_handler import convert_input
-from edge_corner_handler import check_corner
+from edge_corner_handler import check_corner, check_edge
 
-with open('test-heightmap.txt') as h:
+with open('heightmap.txt') as h:
     heightmap_raw = h.readlines()
 
 heightmap = convert_input(heightmap_raw)
-print(heightmap)
-
-# conditions:
-# if row is min or max, don't check -1 row or +1 row, respectively
-# if column (index) is min or max, don't check -1 index or +1 index, respectively
-# seems like 8 special cases: 4 corners and the remainder of the 4 edges
-
-# row min index = 0, row max index = len(heightmap) - 1
-# column min index = 0, column max index = len(heightmap[0]) -1
+# print(heightmap)
 
 
 def find_low_spots():
+    """
+    Finds low points in the heightmap. If the number at any [row][column] coordinate is lower than the four adjacent
+    numbers (diagonals excluded), adds that number to the list of low points. Returns the list of low points.
+    """
+
     row_max = len(heightmap) - 1
     col_max = len(heightmap[0]) - 1
 
@@ -28,8 +25,32 @@ def find_low_spots():
                 corner_value = check_corner(heightmap, i, j)
                 if corner_value is not None:
                     low_points.append(corner_value)
+                edge_value = check_edge(heightmap, i, j)
+                if edge_value is not None:
+                    low_points.append(edge_value)
+            else:
+                rules = [heightmap[i][j] < heightmap[i - 1][j],
+                         heightmap[i][j] < heightmap[i + 1][j],
+                         heightmap[i][j] < heightmap[i][j - 1],
+                         heightmap[i][j] < heightmap[i][j + 1]]
+                if all(rules):
+                    # print(f"inner at [{i}][{j}]: {heightmap[i][j]} is smaller than {heightmap[i - 1][j]},
+                    # {heightmap[i + 1][j]}, " f"{heightmap[i][j - 1]}, and {heightmap[i][j + 1]}")
+                    low_points.append(heightmap[i][j])
+    return low_points
 
-    print(low_points)
+
+def calc_risk_level():
+    """
+    Adds one to each low point value to get the risk level, and returns the sum of all the risk levels for the
+    heightmap.
+    """
+
+    low_points = find_low_spots()
+    risk_level = 0
+    for i in low_points:
+        risk_level += int(i) + 1
+    return risk_level
 
 
-find_low_spots()
+print(f"The low point risk level of this heightmap is: {calc_risk_level()}")
--- dec09/edge_corner_handler.py
+++ dec09/edge_corner_handler.py
@@ -1,4 +1,10 @@
 def check_corner(heightmap, row, col):
+    """
+    Takes a heightmap (a list of strings of numbers, each with the same length), a row number, and a column number.
+    Checks whether the number at [row[column] is a corner of the heightmap. If so, returns the number at the
+    [row][column] coordinate if it is lower than the two adjacent numbers (diagonals excluded).
+    """
+
     row_max = len(heightmap) - 1
     col_max = len(heightmap[0]) - 1
     row_factor = 0
@@ -16,6 +22,54 @@ def check_corner(heightmap, row, col):
     elif row == row_max and col == col_max:
         row_factor = row - 1
         col_factor = col - 1
-    if heightmap[row][col] < heightmap[row_factor][col] and heightmap[row][col] < heightmap[row][col_factor]:
-        return heightmap[row][col]
+    if row_factor != 0 and col_factor != 0:
+        if heightmap[row][col] < heightmap[row_factor][col] and heightmap[row][col] < heightmap[row][col_factor]:
+            # print(f"corner at [{row}][{col}]: {heightmap[row][col]} is smaller than {heightmap[row_factor][col]} and "
+            #       f"{heightmap[row][col_factor]}")
+            return heightmap[row][col]
+    return None
+
+
+def check_edge(heightmap, row, col):
+    """
+    Takes a heightmap (a list of strings of numbers, each with the same length), a row number, and a column number.
+    Checks whether the number at [r][column] is on an edge of the heightmap. If so, returns the number at the
+    [row][column] coordinate if it is lower than the three adjacent numbers (diagonals excluded).
+    """
+
+    row_max = len(heightmap) - 1
+    col_max = len(heightmap[0]) - 1
+    col_check = 0
+    row_check = 0
+    row_factor = 0
+    col_factor = 0
+
+    # top edge except corners
+    if row == 0 and col != 0 and col != col_max:
+        col_check = 2
+        row_factor = row + 1
+    # bottom edge except corners
+    elif row == row_max and col != 0 and col != col_max:
+        col_check = 2
+        row_factor = row - 1
+    # left edge except corners
+    elif col == 0 and row != 0 and row != row_max:
+        row_check = 2
+        col_factor = col + 1
+    # right edge except corners
+    elif col == col_max and row != 0 and row != row_max:
+        row_check = 2
+        col_factor = col - 1
+    if col_check == 2:
+        if heightmap[row][col] < heightmap[row][col - 1] and heightmap[row][col] < heightmap[row][col + 1] and \
+                heightmap[row][col] < heightmap[row_factor][col]:
+            # print(f"t/b edge at [{row}][{col}]: {heightmap[row][col]} is smaller than {heightmap[row][col - 1]}, "
+            #       f"{heightmap[row][col + 1]}, and {heightmap[row_factor][col]}")
+            return heightmap[row][col]
+    if row_check == 2:
+        if heightmap[row][col] < heightmap[row - 1][col] and heightmap[row][col] < heightmap[row + 1][col] and \
+                heightmap[row][col] < heightmap[row][col_factor]:
+            # print(f"l/r edge at [{row}][{col}]: {heightmap[row][col]} is smaller than {heightmap[row - 1][col]}, "
+            #       f"{heightmap[row + 1][col]}, and {heightmap[row][col_factor]}")
+            return heightmap[row][col]
     return None
