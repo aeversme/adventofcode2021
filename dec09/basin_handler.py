2e0249affc1f26af819d55924e4228ad2e59e16c
Alex <aeversme@gmail.com>
2021-12-09 21:39:00 -0500

    add completed dec09_02

--- dec09/basin_handler.py
+++ dec09/basin_handler.py
@@ -1,4 +1,13 @@
+basin_size = 0
+tracked_coordinates = []
+
+
 def is_on_map(heightmap, row, col):
+    """
+    Takes a heightmap and two integer values. Determines if the the row or column values fall outside the index
+    range of the map; if so, returns False. Otherwise (if [row][column] is within the bounds of the map), returns True.
+    """
+
     rules = [row < 0, row > (len(heightmap) - 1), col < 0, col > (len(heightmap[0]) - 1)]
     if any(rules):
         return False
@@ -6,11 +15,47 @@ def is_on_map(heightmap, row, col):
 
 
 def is_previous_coord(previous_list, row, col):
+    """
+    Takes a list and two integer values. Determines if [row, col] is in the list; if so, returns True. Otherwise,
+    returns False.
+    """
+
     for i in previous_list:
-        if i[0] == row and i[1] == col:
+        if i == [row, col]:
             return True
     return False
 
 
-def discover_basin():
-    return
+def discover_basin(heightmap, row, col, is_low_point=False):
+    """
+    Takes a heightmap, two integer values, and a boolean (default = False). If the boolean argument is_low_point is
+    passed in as True (an outside call of the function), sets starting values for basin size and a list of
+    tracked coordinates.
+
+    For the four cardinal directions (up, down, left, right) relative to the given input coordinates [row][column] on
+    the heightmap, determines if: the index of the next coordinates in that direction is within the bounds of the
+    map; if the next coordinates have not already been tracked as part of the basin; and if the value at the next
+    coordinates is not equal to '9'.
+
+    If all three conditions are met, adds one to basin size, adds the next coordinates to the list of tracked
+    coordinates, and calls itself recursively with the next coordinates. If any condition is not met,
+    the loop continues with the next direction. If all directions have been checked, moves back down the stack and
+    continues evaluating directions for the previous coordinates.
+
+    Returns basin size.
+    """
+
+    global basin_size, tracked_coordinates
+    if is_low_point:
+        basin_size = 1
+        tracked_coordinates = [[row, col]]
+    directions = [[row - 1, col], [row + 1, col], [row, col - 1], [row, col + 1]]
+    for direction in directions:
+        r = direction[0]
+        c = direction[1]
+        if is_on_map(heightmap, r, c) and not is_previous_coord(tracked_coordinates, r, c) and not \
+                heightmap[r][c] == '9':
+            basin_size += 1
+            tracked_coordinates.append([r, c])
+            discover_basin(heightmap, r, c)
+    return basin_size
--- dec09/dec09_02.py
+++ dec09/dec09_02.py
@@ -1,11 +1,28 @@
 from input_handler import convert_input
 from low_point_handler import find_low_points
+from basin_handler import discover_basin
 
-with open('test-heightmap.txt') as h:
+with open('heightmap.txt') as h:
     heightmap_raw = h.readlines()
 
-heightmap = convert_input(heightmap_raw)
-# print(heightmap)
 
-low_points = find_low_points(heightmap)
-print(low_points)
+def analyze_map(data_input):
+    """
+    Takes a set of input data. Generates a heightmap list, finds the low points in the map, discovers the basins
+    around each low point, and calculates each basin size. Returns a list of the largest three basins on the map.
+    """
+
+    heightmap = convert_input(data_input)
+
+    low_points = find_low_points(heightmap)
+
+    basins = []
+    for i in range(len(low_points)):
+        basin_size = discover_basin(heightmap, low_points[i][1], low_points[i][2], is_low_point=True)
+        basins.append(basin_size)
+    biggest_three = sorted(basins)[-3:]
+    return biggest_three
+
+
+biggest_basins = analyze_map(heightmap_raw)
+print(f"Product of largest three basins: {biggest_basins[0] * biggest_basins[1] * biggest_basins[2]}")
