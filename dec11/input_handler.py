046ce56424b1012bf683bcda255ce4df4c516d65
Alex <aeversme@gmail.com>
2021-12-11 10:09:12 -0500

    add flash_handler

--- dec11/input_handler.py
+++ dec11/input_handler.py
@@ -2,5 +2,6 @@ def convert_input(data):
     data_strip = [i.strip('\n') for i in data]
     data_split = [list(i) for i in data_strip]
     for i in data_split:
-        i = [int(j) for j in i]
+        for j in range(len(i)):
+            i[j] = int(i[j])
     return data_split
