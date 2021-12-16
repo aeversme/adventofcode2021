916fe1ad2495e71e875e528412bea8954ea023b7
Alex <aeversme@gmail.com>
2021-12-16 01:32:07 -0500

    add dec15_02

--- dec15/map_handler.py
+++ dec15/map_handler.py
@@ -25,6 +25,49 @@ def create_chiton_map(data):
     return chitons
 
 
+def create_chiton_cave(data):
+    chiton_strings = convert_input(data)
+    wrap_dict = {
+        10: 1,
+        11: 2,
+        12: 3,
+        13: 4
+    }
+
+    # iterate map vertically
+    cave_strings = chiton_strings.copy()
+    for i in range(1, 5):
+        for r in range(len(chiton_strings)):
+            new_row = ''
+            for c in range(len(chiton_strings[0])):
+                new_num = int(cave_strings[r][c]) + i
+                if new_num in wrap_dict.keys():
+                    new_num = wrap_dict[new_num]
+                new_row += str(new_num)
+            cave_strings.append(new_row)
+
+    # then iterate horizontally
+    for r in range(len(cave_strings)):
+        for i in range(1, 5):
+            for c in range(len(chiton_strings[0])):
+                new_num = int(cave_strings[r][c]) + i
+                if new_num in wrap_dict.keys():
+                    new_num = wrap_dict[new_num]
+                cave_strings[r] += str(new_num)
+
+    chitons = []
+
+    for r in range(len(cave_strings)):
+        row = []
+        for c in range(len(cave_strings[0])):
+            row.append(Chiton(r, c, cave_strings[r][c]))
+        chitons.append(row)
+
+    chitons[0][0].distance = 0
+
+    return chitons
+
+
 def is_on_map(chiton_map, row, col):
     """
     Takes a two-dimensional list and two integers. Determines if the the row or column values fall outside the index
