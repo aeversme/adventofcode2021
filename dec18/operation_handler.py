cc2c6266f024a65c4c12f164998043a62e118061
Alex <aeversme@gmail.com>
2021-12-18 23:52:46 -0500

    add split and explode functions

--- dec18/operation_handler.py
+++ dec18/operation_handler.py
@@ -19,3 +19,22 @@ def check_for_reduction(number, count=0, reduction=None):
                 reduction_type, reduction_operand, counter = check_for_reduction(pair_half, counter, reduction_type)
     counter -= 1
     return reduction_type, reduction_operand, counter
+
+
+# TODO: have this take the entire 'number', change to string, manipulate, and perform literal_eval()
+def split_number(num, operand):
+    first_half = num // 2
+    second_half = num - first_half
+    return [first_half, second_half]
+
+
+# TODO: make this work
+def explode_pair(num, operand):
+    # str(num)
+    # str(operand)
+    # find index of operand in num_string
+    # split operand
+    # add left value to next regular number (if any) to the left of the exploding pair
+    # add right value to the next regular number (if any) to the right of the exploding pair
+    # perform literal_eval() on new num_string
+    pass
--- dec18/tests/operation_handler_test.py
+++ dec18/tests/operation_handler_test.py
@@ -1,4 +1,4 @@
-from ..operation_handler import add_numbers, check_for_reduction
+from ..operation_handler import add_numbers, check_for_reduction, split_number
 
 
 def test_add_numbers():
@@ -15,7 +15,7 @@ def test_check_for_reduction():
     num1 = [[[[[9, 8], 1], 2], 3], 4]
     num2 = [[6, [5, [4, [3, 2]]]], 1]
     num3 = [[1, 9], [12, 5]]
-    num4 = [[[[0, 7], 4], [15, [0, 13]]], [1, 1]]
+    num4 = [[[[0, 7], 4], [15, [0, [1, 3]]]], [1, 1]]
     num5 = [[[[1, 2], [3, 4]], [[5, 6], [7, 8]]], 9]
 
     assert check_for_reduction(num1) == ('explode', [9, 8], -1)
@@ -23,3 +23,14 @@ def test_check_for_reduction():
     assert check_for_reduction(num3) == ('split', 12, -1)
     assert check_for_reduction(num4) == ('split', 15, -1)
     assert check_for_reduction(num5) == (None, None, -1)
+
+
+# TODO: change to match expected outcome of refactored function, this will fail currently (missing operand parameter)
+def test_split_number():
+
+    assert split_number(10) == [5, 5]
+    assert split_number(11) == [5, 6]
+    assert split_number(17) == [8, 9]
+
+
+# TODO: create test_explode_pair()
