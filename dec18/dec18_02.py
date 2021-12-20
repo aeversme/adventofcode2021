159f8b08cc683f79301eb0e681bd64e4af5add6f
Alex <aeversme@gmail.com>
2021-12-20 13:44:42 -0500

    refactor dec18_02

--- dec18/dec18_02.py
+++ dec18/dec18_02.py
@@ -1,5 +1,5 @@
 from input_handler import convert_input
-from procedure_handler import addition_with_checks, check_for_reduction
+from procedure_handler import addition_with_checks
 from operation_handler import calculate_magnitude
 
 with open('snailfish.txt') as s:
@@ -9,42 +9,28 @@ snailfish_homework = [convert_input(i) for i in snailfish_raw]
 
 
 def find_largest_pair_magnitude(data):
+    data_length = len(data)
     largest_magnitude = 0
-    first_number = None
-    second_number = None
-    for i in range(len(data) - 1):
+    sums_to_check = []
+    for i in range(data_length):
+        print(f"Calculating sums for index {i}/{data_length - 1}...")
         x = data[i]
-        print(f"i: {i}, x: {x}")
-        for j in range(len(data) - 1):
+        # print(f"i: {i}, x: {x}")
+        for j in range(len(data)):
             if j != i:
                 y = data[j]
-                print(f"   j: {j}, y: {y}")
+                # print(f"   j: {j}, y: {y}")
                 sum_x_y = addition_with_checks(x, y)
-                magnitude = calculate_magnitude(sum_x_y)
-                print(f"   x + y: {magnitude}")
-                if magnitude > largest_magnitude:
-                    print(f"== Setting largest found magnitude to {magnitude}")
-                    largest_magnitude = magnitude
-                    first_number = x
-                    second_number = y
-    return largest_magnitude, first_number, second_number
+                sums_to_check.append(sum_x_y)
+    print("Calculating magnitude of sums...")
+    for num in sums_to_check:
+        magnitude = calculate_magnitude(num)
+        # print(f"   x + y: {magnitude}")
+        if magnitude > largest_magnitude:
+            # print(f"== Setting largest found magnitude to {magnitude}")
+            largest_magnitude = magnitude
+    return largest_magnitude
 
 
-largest_mag, first_num, second_num = find_largest_pair_magnitude(snailfish_homework)
-print(f"The largest magnitude is: {largest_mag}\nfrom {first_num} + {second_num}")
-
-# num1 = [[[[5, 2], 2], 5], 5]
-# num2 = [38, 123]
-#
-# sum_nums = addition_with_checks(num1, num2)
-# print(sum_nums)
-#
-# mag = calculate_magnitude(sum_nums)
-# print(mag)
-
-# explode on second '[5, 6]' at index 40
-# num3 = [[[9, [5, 6]], [[8, 8], [0, 9]]], [[[9, [5, 6]], 15], [[8, 3], 8]]]
-#
-# print(check_for_reduction(num3))
-
-# should be [[[9, [5, 6]], [[8, 8], [0, 9]]], [[[14, 0], 21], [[8, 3], 8]]]
+largest_mag = find_largest_pair_magnitude(snailfish_homework)
+print(f"The largest magnitude is: {largest_mag}")
