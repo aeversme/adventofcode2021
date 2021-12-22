7c4276474770f3e7ea0babc6cbbdba2ebee2670e
Alex <aeversme@gmail.com>
2021-12-22 11:50:02 -0500

    refactor find_shared_beacons

--- dec19/dec19_01.py
+++ dec19/dec19_01.py
@@ -6,6 +6,11 @@ with open('test-beacons.txt') as b:
 
 scanners = convert_input(beacons_raw)
 
-diff_int, sum_int = find_shared_beacons(scanners[0], scanners[1])
-print(diff_int)
-print(sum_int)
+shared_s1, shared_s2 = find_shared_beacons(scanners[0], scanners[1])
+
+print(f"shared from scanner 1: {shared_s1}")
+print(f"s1 shares {len(shared_s1)} beacons")
+print(f"shared from scanner 2: {shared_s2}")
+print(f"s2 shares {len(shared_s2)} beacons")
+# print(f"updating scanner x coordinate: {scanner2}")
+# print(f"{shared_s1[0]} from {scanner1} shared with {shared_s1[0].shared_with}")
--- dec19/tests/mapping_handler_test.py
+++ dec19/tests/mapping_handler_test.py
@@ -1,4 +1,4 @@
-from ..mapping_handler import find_shared_beacons
+from ..mapping_handler import find_shared_beacons, find_diff_sum_nums
 from ..input_handler import convert_input
 
 
@@ -9,10 +9,14 @@ def setup():
     return scanners
 
 
-def test_find_shared_beacons():
+def test_find_diff_sum_nums():
     scanners = setup()
-    diff_int, sum_int = find_shared_beacons(scanners[0], scanners[1])
+    diff_int, sum_int = find_diff_sum_nums(scanners[0], scanners[1])
 
     assert diff_int == {68, 1133}
     assert sum_int == {864, 1133, 957, 919}
+
+
+def test_find_shared_beacons():
+    assert True
 
