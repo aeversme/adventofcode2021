f4aaa16f67a8188f5160caf3c39838b8fc54bd11
Alex <aeversme@gmail.com>
2022-01-07 00:19:50 -0500

    refactor dec21_02 game_turn

--- dec21/dec21_02.py
+++ dec21/dec21_02.py
@@ -5,9 +5,7 @@ with open('test-dirac.txt') as d:
     start_raw = d.readlines()
 
 
-def game_turn(player_num, players, score_p1, score_p2, space_p1, space_p2, level):
-    stack_level = level
-    player_index = player_num
+def game_turn(player_index, players, score_p1, score_p2, space_p1, space_p2, stack_level):
     player = players[player_index]
     scores = [score_p1, score_p2]
     spaces = [space_p1, space_p2]
@@ -15,27 +13,21 @@ def game_turn(player_num, players, score_p1, score_p2, space_p1, space_p2, level
                                 f"points on {spaces[1]}")
     for roll in range(1, 4):
         print(" . " * stack_level + f"{player_index} rolls {roll}...")
-        spaces[player_index] += roll
-        if spaces[player_index] > 10:
-            spaces[player_index] = spaces[player_index] % 10
-        score = spaces[player_index]
+        spaces[player_index] = (spaces[player_index] + roll) % 10
+        score = spaces[player_index] + 1
         scores[player_index] += score
         print(" . " * stack_level + f"{player_index} moves to {spaces[player_index]}; new score: "
                                     f"{scores[player_index]}")
 
         if max(scores) < 21:
             player_index = (player_index + 1) % 2
-            stack_level += 1
-            game_turn(player_index, players, scores[0], scores[1], spaces[0], spaces[1], stack_level)
+            game_turn(player_index, players, scores[0], scores[1], spaces[0], spaces[1], stack_level + 1)
         else:
             print(" . " * stack_level + f"*** Winner index {player_index} with {scores[player_index]} on "
                                         f"{spaces[player_index]}")
             player.victories += 1
-            scores[player_index] -= score
-            spaces[player_index] -= roll
-            if spaces[player_index] < 0:
-                spaces[player_index] = 10 - spaces[player_index]
-    stack_level -= 1
+            scores = [score_p1, score_p2]
+            spaces = [space_p1, space_p2]
     return
 
 
@@ -43,7 +35,7 @@ def dirac_game(data):
     player_list = convert_input(data)
     players = [Player(player[0], player[1]) for player in player_list]
 
-    game_turn(0, players, 0, 0, 4, 8, 0)
+    game_turn(0, players, 0, 0, 3, 7, 0)
 
     print(f"{players[0].name} has victories in {players[0].victories} universes.")
     print(f"{players[1].name} has victories in {players[1].victories} universes.")
